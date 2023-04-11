efc8cc4fc909746983efa6869c93b5519ddd6dc2
SinduAditya <nduujanadi51@gmail.com>
2023-04-11 14:22:59 +0700

    reset password

--- resources/views/auth/password/edit.blade.php
+++ resources/views/auth/password/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-@section('title', 'Contact App | Login')
+@section('title', 'Contact App | Reset Password')
 
 @section('content')
 
@@ -7,18 +7,17 @@
     <div class="auth-wrapper d-flex bg-light">
         <div class="col-md-4 m-auto">
             <div class="bg-white shadow-sm">
-                <h1 class="border-bottom p-4">Login</h1>
+                <h1 class="border-bottom p-4">Register</h1>
 
                 <div class="px-4 pt-4">
-
-                    <form action="{{ route('login') }}" method="POST">
+                    <form action="{{ route('password.update') }}" method="POST">
                         @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
                             <input type="email"
                                 class="form-control @error('email') is-invalid
                             @enderror"
-                                name="email" value="{{ old('email') }}" />
+                                name="email" value="{{ request()->email }}" />
                             @error('email')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
@@ -31,22 +30,13 @@
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="mb-3 d-flex justify-content-between align-items-center">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" name="remember" value="true"
-                                    id="customCheck1">
-                                <label class="custom-control-label text-black-50" for="customCheck1">Remember me</label>
-                            </div>
-                            {{-- forget password --}}
-                            <a href="{{ route('password.request') }}">Forget your password?</a>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password Confirmation</label>
+                            <input type="password" class="form-control" name="password_confirmation" />
+                            <input type="hidden" name="token" value="{{ request()->route('token') }}">
                         </div>
-                        <div class="mt-4 d-grid">
-                            <button type="submit" class="btn btn-block btn-primary">login</button>
-                            <div class="text-center py-4 text-muted">
-                                Don't have account?
-                                <a href="{{ route('register') }}"
-                                    class="text-muted font-weight-bold text-decoration-none">Register</a>
-                            </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-block btn-primary">Reset Password</button>
                         </div>
                     </form>
                 </div>
