eac749db6af4b72860d3d3bd01c386b386934798
SinduAditya <nduujanadi51@gmail.com>
2023-04-15 16:33:06 +0700

    debugging sql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFour();
+          if (config('app.debug')) {
+            DB::listen(fn ($query) => Log::info($query->sql, $query->bindings, $query->time));
+        }
     }
 }
